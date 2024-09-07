e0215b5b5cf28d106e67c7d7b39a303d03882bcf
Prunkles <prunkles.dreemurr@gmail.com>
2024-09-07 07:23:11 +0300

    Update ipni-index-provider from 0.15.4 to 0.15.5

--- ipni-index-provider.nix
+++ ipni-index-provider.nix
@@ -1,16 +1,16 @@
 { buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "ipni-index-provider";
-  version = "0.15.4";
+  version = "0.15.5";
   rev = "v${version}";
 
   src = fetchFromGitHub {
     owner = "ipni";
     repo = "index-provider";
     inherit rev;
-    hash = "sha256-Q/JNtw5Z3IUojg6Yewp12cxFoR2csNyO0mPK6EmmoSU=";
+    hash = "sha256-EGYnwuW+tHSNdMhwNs5ctekmK5usd6uqDYdxbEe2oqI=";
   };
 
-  vendorHash = "sha256-fPDhnZNW1QF+jUiVz/ZjAxXG/kSPGBoIQ2gNbT0PrrA=";
+  vendorHash = "sha256-lOzXM+fnf2/m6q2mW4CbQSF7W2xl4t/KS+hKPPu5Bjw=";
 }
 
